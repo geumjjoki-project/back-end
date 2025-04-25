696a8453e43bccae051e6441d2c86acc6f7e7c72
SSM <nomio4001@naver.com>
2025-04-25 11:41:58 +0900

    feat: 지출내역 조회기능 추가

---
+++ expenses/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import Expense, ExpenseAnalysis, Category
+
+
+# 카테고리를 재귀적으로 반환
+class RecursiveCategorySerializer(serializers.ModelSerializer):
+    children = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ["id", "name", "children"]
+
+    def get_children(self, obj):
+        children = obj.child_category.all()
+        if children.exists():
+            return RecursiveCategorySerializer(children, many=True).data
+        return []
+
+
+# 지출내역에 들어가는 카테고리 목록
+class InlineCategorySerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source="category_id")
+    parent = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ["id", "name", "parent"]
+
+    def get_parent(self, obj):
+        return obj.parent_category.name if obj.parent_category else None
+
+
+# 전체지출내역
+class ExpenseSerializer(serializers.ModelSerializer):
+    category = InlineCategorySerializer()
+    amount = serializers.SerializerMethodField()
+
+    def get_amount(self, obj):
+        return int(obj.amount)
+
+    class Meta:
+        model = Expense
+        fields = (
+            "expense_id",
+            "date",
+            "amount",
+            "category",
+            "description",
+        )
