af6cefb4ddccd5155986b6c3b16fba9044a9fff3
SSM <nomio4001@naver.com>
2025-05-02 15:37:36 +0900

    feat: 리워드 상세조회 기능 추가

--- rewards/urls.py
+++ rewards/urls.py
@@ -5,6 +5,7 @@ app_name = "rewards"
 
 urlpatterns = [
     path("", views.reward_list, name="reward_index"),
+    path("<int:reward_id>/", views.reward_detail, name="reward_detail"),
     path(
         "list/<int:reward_transaction_id>/",
         views.reward_transaction_detail,
